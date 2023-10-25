4d429a3cbd8520ce6d052768e49eacfa5a2cb31a
JF-117 <121064965+JF-117@users.noreply.github.com>
2023-10-25 13:16:43 -0400

    1 Round Feature Complete

--- Assets/01-Prospector/__Scripts/Card.cs
+++ Assets/01-Prospector/__Scripts/Card.cs
@@ -77,6 +77,12 @@ public class Card : MonoBehaviour {
 			back.SetActive(!value);
 		}
 	}
+
+	// Virtual methods can be overridden by subclass methods with the same name
+	virtual public void OnMouseUpAsButton() {
+	print (name); // When clicked, this outputs the card name
+	}
+
 } // class Card
 
 [System.Serializable]
